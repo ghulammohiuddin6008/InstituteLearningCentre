febd8ac7283ac1cf5249eb92561f6790baabd278
Ghulam Mohiuddin <ghulammohiuddin6008@gmail.com>
2021-04-05 09:20:10 -0700

    upload to gitHub

---
+++ app.js
@@ -0,0 +1,32 @@
+var gender="male"
+function funcmale(){
+    gender="male"
+}
+function funcfemale(){
+    gender="female"
+}
+
+function Objectstudent(stname,fname,cnic,dob,gender,cellno,whatsappno,address){
+    this.Stname=stname
+    this.Fname=fname
+    this.Cnic=cnic
+    this.Dob=dob
+    this.Cellno=cellno
+    this.Whatsappno=whatsappno
+    this.address=address
+    this.Gender=gender
+
+}
+function funcsubmit(){
+    var stname=document.getElementById("txtstname").value
+    var fname=document.getElementById("txtfname").value 
+    var cnic=document.getElementById("txtcnic").value
+    var dob=document.getElementById("txtdob").value
+    var cellno=document.getElementById("txtcellno").value
+    var whatsappno=document.getElementById("txtwhatsappno").value
+    var address=document.getElementById("txtaddress").value 
+    var objst= new Objectstudent(stname,fname,cnic,dob,gender,cellno,whatsappno,address)
+    console.log (objst)
+}
+
+    
